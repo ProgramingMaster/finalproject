0ee75f16d0bb60e53099b56b7a931a6e78ec2226
Quinn <quinnkoch1@gmail.com>
2019-11-26 23:00:07 +0000

    asd

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 from os import environ
+import redis
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -26,9 +27,11 @@ def after_request(response):
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"] = "redis"
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'dev_key'
 Session(app)
+
+
 
 #db = SQL("postgres://zcjxmflvvdjgej:842176674c37fbc83dcc95627716e96dfaf311b1f8b67a50ec52395ee7a5fcbf@ec2-23-21-249-0.compute-1.amazonaws.com:5432/d6dvfncect3bc")
 db = SQL("sqlite:///finalproject.db")
