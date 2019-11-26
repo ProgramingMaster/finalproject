2f0080209ec6edcbdfb73b08e32bb57d2cb72c77
Quinn <quinnkoch1@gmail.com>
2019-11-26 23:03:16 +0000

    asdf

--- application.py
+++ application.py
@@ -25,16 +25,18 @@ def after_request(response):
     return response
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "redis"
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'dev_key'
+SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
+
 Session(app)
 
 
 
-#db = SQL("postgres://zcjxmflvvdjgej:842176674c37fbc83dcc95627716e96dfaf311b1f8b67a50ec52395ee7a5fcbf@ec2-23-21-249-0.compute-1.amazonaws.com:5432/d6dvfncect3bc")
-db = SQL("sqlite:///finalproject.db")
+db = SQL("postgres://zcjxmflvvdjgej:842176674c37fbc83dcc95627716e96dfaf311b1f8b67a50ec52395ee7a5fcbf@ec2-23-21-249-0.compute-1.amazonaws.com:5432/d6dvfncect3bc")
+#db = SQL("sqlite:///finalproject.db")
 
 @app.route("/", methods=["GET", "POST"])
 @login_required
