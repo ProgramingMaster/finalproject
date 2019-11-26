81e993384b91dd5d90158f20e39c35a121a9de24
Quinn <quinnkoch1@gmail.com>
2019-11-26 22:24:11 +0000

    asdf

--- application.py
+++ application.py
@@ -22,14 +22,16 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# # Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+SECRET_KEY = 'asdfghjkl;zxcvnm,.qwertyuiop'
 Session(app)
 
-db = SQL("postgres://zcjxmflvvdjgej:842176674c37fbc83dcc95627716e96dfaf311b1f8b67a50ec52395ee7a5fcbf@ec2-23-21-249-0.compute-1.amazonaws.com:5432/d6dvfncect3bc")
-#db = SQL("sqlite:///finalproject.db")
+#db = SQL("postgres://zcjxmflvvdjgej:842176674c37fbc83dcc95627716e96dfaf311b1f8b67a50ec52395ee7a5fcbf@ec2-23-21-249-0.compute-1.amazonaws.com:5432/d6dvfncect3bc")
+db = SQL("sqlite:///finalproject.db")
 
 @app.route("/", methods=["GET", "POST"])
 @login_required
