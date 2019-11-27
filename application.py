dcf5ce935c2e189fcd40c5fa2403f5f8d91f8a6a
Quinn <quinnkoch1@gmail.com>
2019-11-27 19:51:29 +0000

    asd

--- application.py
+++ application.py
@@ -23,12 +23,14 @@ def after_request(response):
     return response
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+# app.config["SESSION_TYPE"] = "filesystem"
+    app.secret_key = 'asdjfklajsfd'
+    app.config['SESSION_TYPE'] = 'filesystem'
 #app.config.from_mapping(
 #app.secret_key = 'm3vhIaDWWrAp3QlMwjwk'
-SECRET_KEY = 'm3vhIaDWWrAp3QlMwjwk'
+#SECRET_KEY = 'm3vhIaDWWrAp3QlMwjwk'
 #)
 #SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
 
