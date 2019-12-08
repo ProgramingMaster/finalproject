a4fbbf6ca23cbf8181b0761f508b487e5c75a480
Quinn <quinnkoch1@gmail.com>
2019-12-08 21:13:50 +0000

    asdf

--- application.py
+++ application.py
@@ -105,7 +105,7 @@ def index():
         return redirect("/")
 
     else:
-        # Get all of the user's workouts
+        # Get all of the user's workouts in alphabetical order
         workouts = db.execute("SELECT * FROM workouts WHERE userId = :userId ORDER BY name", userId=session["userId"])
 
         return render_template("index.html", workouts=workouts)
